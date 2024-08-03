bc8dc825959c56ce953e33fe8e7b618d0dab6ffa
Shaar <40307680+Shaar-games@users.noreply.github.com>
2024-08-03 13:05:17 +0200

    Update foo.lua

--- foo.lua
+++ foo.lua
@@ -6,5 +6,12 @@ local function foo()
     end
 end
 
+local function rfoo()
+    print("foo")
+    local function f ()
+        print("f")
+    end
+end
+
 
 --local a , b , c , d = foo(1,false,3,4,5)
